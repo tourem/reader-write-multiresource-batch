d2de67c670c4e278946a245ac3680e5bd447eac0
Mahamadou TOURE <touremohameda@gmail.com>
2020-09-03 10:31:45 +0200

    Update MainBatch.java

--- src/main/java/com/larbotech/batch/MainBatch.java
+++ src/main/java/com/larbotech/batch/MainBatch.java
@@ -1,5 +1,8 @@
 package com.larbotech.batch;
 
+
+
+import com.larbotech.batch.exception.BadParameterizedStepJobException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
@@ -13,33 +16,57 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+import java.util.Date;
+
+import static com.larbotech.batc.model.BatchArgumentEnum.BATCH_NAME_ARG;
+
 @Slf4j
 @SpringBootApplication
 public class MainBatch implements CommandLineRunner {
 
-  @Autowired
-  JobLauncher jobLauncher;
+    public static final String TIMESTAMP_PARAM_NAME = "date";
 
-  @Autowired
-  Job job;
+    @Autowired
+    private ApplicationArguments applicationArguments;
 
-  public static void main(String[] args) {
-    SpringApplication.run(MainBatch.class, args);
-  }
+    @Autowired
+    JobLauncher jobLauncher;
 
-  @Override
-  public void run(String... args) {
-    int exitStatus = 0;
-    try {
-      ExitCodeMapper exitCodeMapper = new SimpleJvmExitCodeMapper();
-      JobParameters jobParameters = new JobParametersBuilder()
-          .addLong("time", System.currentTimeMillis())
-          .toJobParameters();
-      JobExecution jobExecution = jobLauncher.run(job, jobParameters);
-      exitStatus = exitCodeMapper.intValue(jobExecution.getExitStatus().getExitCode());
-    } catch (Exception e) {
-      log.error(e.getMessage(), e);
+    @Autowired
+    Job job;
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainBatch.class, args);
     }
-    System.exit(exitStatus);
-  }
+
+    @Override
+    public void run(String... args) {
+
+        log.info("Application started with command-line arguments: {}", Arrays.toString(applicationArguments.getSourceArgs()));
+        log.info("NonOptionArgs: {}", applicationArguments.getNonOptionArgs());
+        log.info("OptionNames: {}", applicationArguments.getOptionNames());
+
+        if (applicationArguments.containsOption(BATCH_NAME_ARG.getArgumentName())) {
+
+            String batchName = applicationArguments.getOptionValues(BATCH_NAME_ARG.getArgumentName()).get(0);
+
+            JobParameters param = new JobParametersBuilder()
+                    .addDate(TIMESTAMP_PARAM_NAME, new Date())
+                    .addString(BATCH_NAME_ARG.getArgumentName(), batchName)
+                    .toJobParameters();
+
+            int exitStatus = 0;
+            try {
+                ExitCodeMapper exitCodeMapper = new SimpleJvmExitCodeMapper();
+                JobExecution jobExecution = jobLauncher.run(job, param);
+                exitStatus = exitCodeMapper.intValue(jobExecution.getExitStatus().getExitCode());
+            } catch (Exception e) {
+                log.error(e.getMessage(), e);
+            }
+            System.exit(exitStatus);
+            return;
+        }
+        throw new BadParameterizedStepJobException("Missing --" + BATCH_NAME_ARG.getArgumentName() + "=argument");
+    }
 }
