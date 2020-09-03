40322f7978351b81aa2a2e6998870bc7203a84df
Mahamadou TOURE <touremohameda@gmail.com>
2020-09-03 10:12:11 +0200

    Create BadParameterizedStepJobException.java

---
+++ src/main/java/com/larbotech/batch/exception/BadParameterizedStepJobException.java
@@ -0,0 +1,21 @@
+package com.larbotech.batch.exception;
+
+public class BadParameterizedStepJobException extends BatchException {
+
+    static final long serialVersionUID = 1L;
+
+    public BadParameterizedStepJobException() {
+    }
+
+    public BadParameterizedStepJobException(String message) {
+        super(message);
+    }
+
+    public BadParameterizedStepJobException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public BadParameterizedStepJobException(Throwable cause) {
+        super(cause);
+    }
+}
